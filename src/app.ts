fd3e3c0162763524d834a2465365ba939cf1e3a8
Nsikan Akpan <nsikan.akpan@betmakers.com>
2023-12-22 15:13:18 -0500

    login init

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+
+import * as middlewares from "./middlewares/middlewares";
+import QuoteController from "./api/v1/quote/QuoteController";
+import MessageResponse from "./interfaces/MessageResponse";
+import ReportController from "./api/v1/report/ReportController";
+import verifyToken from "./middlewares/verifyToken";
+import LoginController from "./api/v1/login/LoginController";
+
+require("dotenv").config();
+
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get<{}, MessageResponse>("/", (req, res) => {
+  res.json({
+    message: "Hello World",
+  });
+});
+
+app.use("/api/v1/login", LoginController);
+app.use("/api/v1/quotes", QuoteController);
+app.use("/api/v1/reports", ReportController);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+export default app;
