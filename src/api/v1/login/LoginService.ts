8671e599a4f45aa7720f8a57f9f6823fc0057f87
Nsikan Akpan <nsikan.akpan@betmakers.com>
2024-01-18 00:29:03 -0500

    removed code

--- src/api/v1/login/LoginService.ts
+++ src/api/v1/login/LoginService.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import _ from "lodash";
 import { useLoginRepo } from "./LoginRepo";
 
-export const EXPIRATION_TIME = "1hr";
+export const EXPIRATION_TIME = "2hr";
 
 export const useLoginService = () => {
   const login = async ({ username, password }: any) => {
