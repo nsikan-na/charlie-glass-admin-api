567b7da7a7a95786bbd869ccd65aedc22101a692
Nsikan Akpan <nsikan.akpan@betmakers.com>
2024-02-17 15:24:09 -0500

    changed route

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,9 @@
 import express from "express";
-import morgan from "morgan";
-import helmet from "helmet";
-import cors from "cors";
-
-import * as middlewares from "./middlewares/middlewares";
-import InvoiceController from "./api/v1/invoice/InvoiceController";
 import MessageResponse from "./interfaces/MessageResponse";
-import ReportController from "./api/v1/report/ReportController";
-import verifyToken from "./middlewares/verifyToken";
-import LoginController from "./api/v1/login/LoginController";
-
+import cgiAdminRouter from "./cgi-admin";
 require("dotenv").config();
 
 const app = express();
-
-app.use(morgan("dev"));
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
 
 app.get<{}, MessageResponse>("/", (req, res) => {
   res.json({
@@ -25,11 +11,6 @@ app.get<{}, MessageResponse>("/", (req, res) => {
   });
 });
 
-app.use("/api/v1/login", LoginController);
-app.use("/api/v1/invoices", verifyToken, InvoiceController);
-app.use("/api/v1/reports", verifyToken, ReportController);
-
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
+app.use("/cgi-admin", cgiAdminRouter);
 
 export default app;
---
+++ src/cgi-admin/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+
+import * as middlewares from "../middlewares/middlewares";
+import InvoiceController from "../api/v1/invoice/InvoiceController";
+import MessageResponse from "../interfaces/MessageResponse";
+import ReportController from "../api/v1/report/ReportController";
+import verifyToken from "../middlewares/verifyToken";
+import LoginController from "../api/v1/login/LoginController";
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get<{}, MessageResponse>("/", (req, res) => {
+  res.json({
+    message: "Hello World",
+  });
+});
+
+app.use("/api/v1/login", LoginController);
+app.use("/api/v1/invoices", verifyToken, InvoiceController);
+app.use("/api/v1/reports", verifyToken, ReportController);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+export default app;
